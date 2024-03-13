98400bff053f5c511a201d36a8761b225af39d3d
NITAAIPractitioners <162414682+NITAAIPractitioners@users.noreply.github.com>
2024-03-13 10:44:28 +0000

    python dictionary inside html

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+students = [
+{'id':1,
+'sname':'Abdalla Alqahtani',
+'insterests':'Web scraping'},
+ { 'id':2,
+  'sname':'Hussain',
+  'insterests':'project engineer'},
+  { 'id':3,
+  'sname':'Ali Alqahtani',
+  'insterests':'AI modelling'},
+  { 'id':4,
+  'sname':'Majed Almutairy',
+  'insterests':'Data engineer'}
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',students=students)
 
 
 if __name__ == '__main__':
