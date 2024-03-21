deddb8c8cb496cdbdbf43f4c6afcbb9663adb41b
NITAAIPractitioners <162414682+NITAAIPractitioners@users.noreply.github.com>
2024-03-21 08:36:37 +0000

    build contact us and details buttons.

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from database import load_students_from_db, load_student_from_db, add_message_to_db
 
 app = Flask(__name__)
-students = [
-{'id':1,
-'sname':'Abdalla Alqahtani',
-'insterests':'Web scraping'},
- { 'id':2,
-  'sname':'Hussain',
-  'insterests':'project engineer'},
-  { 'id':3,
-  'sname':'Ali Alqahtani',
-  'insterests':'AI modelling'},
-  { 'id':4,
-  'sname':'Majed Almutairy',
-  'insterests':'Data engineer'}
-]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html',students=students)
+  projects = load_students_from_db()
+  return render_template('home.html', students=projects)
+
+
+@app.route("/project/<id>")
+def project_details(id):
+  project = load_student_from_db(id)
+  return render_template('project_details.html', project=project)
+
+
+@app.route('/message')
+def message():
+  return render_template('contactus.html')
+
+
+@app.route("/message_submitted", methods=['post'])
+def message_submitted():
+  mdata = request.form
+  add_message_to_db(mdata)
+  return render_template('message_submitted.html', data=mdata)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,9 +1,30 @@
 from sqlalchemy import create_engine, text
+import os
 
-engine = create_engine(
-    "mysql+pymysql://admin:1234admin@database-3.cnmq0wo4g64z.eu-north-1.rds.amazonaws.com/bmembers?charset=utf8mb4"
-)
-with engine.connect() as conn:
-  result = conn.execute(text("select * from trial"))
-  print(result.all())
-#interaction between mysql and python
+engine = create_engine(os.environ['DB_CONNECTION_STRING'])
+
+
+def load_students_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from Mystudents"))
+    projects = []
+    for row in result.all():
+      projects.append(dict(row._mapping))
+  return projects
+
+
+def load_student_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"select * from Mystudents where id = {id}"))
+    rows = result.all()
+    return dict(rows[0]._mapping)
+
+
+def add_message_to_db(data):
+  with engine.connect() as conn:
+    sql = text(
+        f"INSERT INTO mymessages (full_name, email, message) VALUES (\'{data['full_name']}\', \'{data['email']}\', \'{data['message']}\')"
+    )
+    print(sql)
+    conn.execute(sql)
+    conn.commit()
